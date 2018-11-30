1ac91e3da832909e10e8726253e9d4a893394a22
Jon McDuffie <jon.mcduffie@vonage.com>
2018-11-30 07:10:08 +0000

    Setup static server

---
+++ index.js
@@ -0,0 +1,7 @@
+const static = require('node-static');
+const file = new static.Server('./dist');
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
