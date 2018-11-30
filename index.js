48a860736128b4b636a6bbadfc809acb42b5a540
Jon McDuffie <jon.mcduffie@vonage.com>
2018-11-30 07:52:45 +0000

    Use express

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const static = require('node-static');
-const file = new static.Server('./dist');
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/openTok.env.json', function (req, res) {
+  const apiKey = process.env.API_KEY;
+  const sessionId = process.env.SESSION_ID;
+  const token = process.env.TOKEN;
+
+  // Respond to the client
+  res.json({
+    "credentials": {
+      "apiKey": apiKey,
+      "sessionId": sessionId,
+      "token": token
+    }
+  });
+});
+app.use(express.static('dist'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
